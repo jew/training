71825c63600a2e2bfb405e54e73c2da6becd163c
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-24 10:55:05 +0700

    Edited hei subroutine#2

--- lib/Hello.pm
+++ lib/Hello.pm
@@ -1,11 +1,10 @@
 package Hello;
 use strict;
-&hei(@_);
 sub hei {
      my ($name) = @_; 
      return "Hello $name\n";
 }
 1;
-=head2 <hei>
+=head2 hei
 hei() is subroutine that get name from command line and return Hello and name that get from command line 
 =cut
