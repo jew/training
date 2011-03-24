70935d7baeaededa97d0992af220f725fa6f7f20
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-24 15:49:55 +0700

    Edited hei subroutine#2

--- lib/Hello.pm
+++ lib/Hello.pm
@@ -1,12 +1,13 @@
 package Hello;
 use strict;
 sub hei {
-     my ($name) = @_; 
-	if($name eq "Bjorn"){
-		return ("Hei $name");
-	}else{ return ("Hello $name");}
+    my ($name) = @_; 
+    if($name eq "Bjorn"){
+        return ("Hei $name");
+	}else{
+	 	return ("Hello $name");
+	}
 }
-
 1;
 =head2 hei
 hei() is subroutine that get name from command line and return Hello and name that get from command line 
