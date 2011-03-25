1e062f9308b71bbf2d82add35749fe7a6462f387
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-25 16:22:51 +0700

    Edit downloadpage  Script#6

--- script/downloadpage.pl
+++ script/downloadpage.pl
@@ -1,3 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+use DownLoadPage;
+
+my $url = $ARGV[0];
+my $dlp = DownLoadPage->new(url => $url);
+
+print "Your page: \n";
+print $dlp->download_to_memory;
+print "\n";
