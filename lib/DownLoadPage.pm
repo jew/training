4d90cb1d103a112977edd89e51b6319f0a217a39
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-25 11:49:35 +0700

    Module #6

--- lib/DownLoadPage.pm
+++ lib/DownLoadPage.pm
@@ -1,7 +1,12 @@
 package DownLoadPage;
 use strict;
 use warnings;
+use Moose;
+
 sub downLoadPage {
 
 }
 1;
+=head2 DownLoadPage
+    DownLoadPage() is a subroutine that use for downloading page.
+=cut
