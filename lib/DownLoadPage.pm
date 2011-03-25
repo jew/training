fed4998bb17492845dea2dbbc290f07afa077250
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-25 16:18:53 +0700

    Module #6

--- lib/DownLoadPage.pm
+++ lib/DownLoadPage.pm
@@ -2,9 +2,19 @@ package DownLoadPage;
 use strict;
 use warnings;
 use Moose;
+use LWP::UserAgent;
 
-sub downLoadPage {
+has 'url' => ( is => 'rw', isa => 'Str', required => 1, );
+has '_ua' => ( is => 'ro', isa => 'LWP::UserAgent', lazy_build => 1, );
 
+sub _build__ua {
+    my ($self) = @_;
+    return LWP::UserAgent->new;
+}
+
+sub download_to_memory {
+    my ($self) = @_;
+    return $self->url;
 }
 1;
 =head2 DownLoadPage
