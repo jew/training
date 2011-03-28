85ae6595b256b217f006d1c1c2f999fe7ec595cc
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-28 17:39:42 +0700

    Edit DownLoadPage.pm #6

--- lib/DownLoadPage.pm
+++ lib/DownLoadPage.pm
@@ -7,34 +7,41 @@ use File::Slurp ;
 
 has 'url' => ( is => 'rw', isa => 'Str', required => 1, );
 has '_ua' => ( is => 'ro', isa => 'LWP::UserAgent', lazy_build => 1, );
-
+my $decode1;
+my $counter = 0;
 sub _build__ua {
     my ($self) = @_;
     return LWP::UserAgent->new;
 }
-
 sub download_to_memory {
-    my ($self) = @_;
-    my $response = $self->_ua->get($self->url);
-    my $decode = $response->decoded_content;
-    return $decode;
+    if($counter==0) {
+        my ($self) = @_;
+        my $response = $self->_ua->get($self->url);
+        my $counter1 ++; 
+        my $decode = $response->decoded_content;
+        $decode1= $decode;
+        return $decode1;
+    }
+    else {
+        return $decode1;
+    }
 }
 sub write_to_file {
     my ($self, $filename) = @_;
-    my $text = $self->download_to_memory;
+    my $text = $decode1;
     write_file($filename,{binmode => ':utf8'},$text);
 }
 sub download_to_file {
-    my ($self, $filename) = @_;
-    if(&download_to_memory ne '') {
-        $self->download_to_memory;
+   my ($self, $filename) = @_;
+   if($counter==1) {
         $self->write_to_file($filename);
-    }
-    else {
-        $self->write_to_file($filename);
-    }   
+   }
+   else {
+       $self->download_to_memory;
+       $self->write_to_file($filename);
+   }
 }
 1;
-=head2 DownLoadPage
+=head2 _build__au
     
 =cut
