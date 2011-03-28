86988b5ba5037a4e688d5d397e5d9383fcf0c30b
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-28 14:49:41 +0700

    Edit downloadpage Script#6

--- script/downloadpage.pl
+++ script/downloadpage.pl
@@ -2,11 +2,22 @@
 use strict;
 use warnings;
 
+use FindBin;
+use lib "$FindBin::Bin/../lib";
 use DownLoadPage;
 
 my $url = $ARGV[0];
+my $filename = $ARGV[1];
 my $dlp = DownLoadPage->new(url => $url);
-
 print "Your page: \n";
 print $dlp->download_to_memory;
 print "\n";
+    if( $dlp->write_to_file($filename)==1) {
+        print "SAVE FILE : OK\n";
+    }
+    else {
+            print "Can not write";
+    }      
+    
+print $dlp->download_to_file($filename);
+   
