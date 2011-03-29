ded6d6e5acab3f3c86c25d0b09b57fd734bf66c7
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-29 09:57:57 +0700

    Edit downloadpage Script#6

--- script/downloadpage.pl
+++ script/downloadpage.pl
@@ -10,13 +10,21 @@ my $url = $ARGV[0];
 my $filename = $ARGV[1];
 my $dlp = DownLoadPage->new(url => $url);
 print "Your page: \n";
-print $dlp->download_to_memory;
+print $dlp->_build_content;
+print "build content";
 print "\n";
-if( $dlp->write_to_file($filename)==1) {
+if($dlp->download_to_memory ne '') {
+    print "Downloading successfull";
+}
+else {
+    print " Not Successfull";
+}
+print "\n";
+if($dlp->write_to_file($filename)==1) {
     print "SAVE FILE : OK\n";
 }
 else {
     print "Can not write";
 }         
-print $dlp->download_to_file;
+print $dlp->download_to_file($filename);
    
