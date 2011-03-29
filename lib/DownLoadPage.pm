b130f7fe56657751e3b00d39aff6af121fc1643c
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-29 09:59:28 +0700

    Edit DownLoadPage.pm #6

--- lib/DownLoadPage.pm
+++ lib/DownLoadPage.pm
@@ -7,39 +7,35 @@ use File::Slurp ;
 
 has 'url' => ( is => 'rw', isa => 'Str', required => 1, );
 has '_ua' => ( is => 'ro', isa => 'LWP::UserAgent', lazy_build => 1, );
-my $decode1;
-my $counter = 0;
+has 'content' => ( is => 'ro', isa => 'Str', lazy_build => 1, );
 sub _build__ua {
     my ($self) = @_;
     return LWP::UserAgent->new;
 }
+sub _build_content {
+    my ($self) = @_;
+    my $response = $self->_ua->get($self->url);
+    my $text = $response->decoded_content;
+    return $text;
+}
 sub download_to_memory {
-    if($counter==0) {
-        my ($self) = @_;
-        my $response = $self->_ua->get($self->url);
-        my $counter1 ++; 
-        my $decode = $response->decoded_content;
-        $decode1= $decode;
-        return $decode1;
-    }
-    else {
-        return $decode1;
-    }
+    my ($self) = @_;
+    return $self->content;
 }
 sub write_to_file {
     my ($self, $filename) = @_;
-    my $text = $decode1;
+    my $text = $self->content;
     write_file($filename,{binmode => ':utf8'},$text);
 }
 sub download_to_file {
-   my ($self, $filename) = @_;
-   if($counter==1) {
+    my ($self, $filename) = @_;
+    if($self->content ne '') {
         $self->write_to_file($filename);
-   }
-   else {
-       $self->download_to_memory;
-       $self->write_to_file($filename);
-   }
+    }
+    else {
+        $self->download_to_memory($self->content);
+        $self->write_to_file($filename);
+    }
 }
 1;
 =head2 _build__au
