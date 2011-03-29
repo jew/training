79c6963847766946de94d54c37f70e4d83aa183b
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-03-29 11:51:16 +0700

    Edit downloadpage Script#6

--- script/downloadpage.pl
+++ script/downloadpage.pl
@@ -8,23 +8,29 @@ use DownLoadPage;
 
 my $url = $ARGV[0];
 my $filename = $ARGV[1];
-my $dlp = DownLoadPage->new(url => $url);
-print "Your page: \n";
-print $dlp->content;
-print "build content";
-print "\n";
-if($dlp->download_to_memory ne '') {
-    print "Downloading successfull";
+print "URL IS :$url\n";
+if(($url) ne ('')) {
+    my $dlp = DownLoadPage->new(url => $url);
+    print "Your page: \n";
+    print $dlp->content;
+    print "build content";
+    print "\n";
+    if($dlp->download_to_memory ne '') {
+        print "Downloading successfull";
+    }
+    else {
+        print " Not Successfull";
+    }
+    print "\n";
+    if($dlp->write_to_file($filename)==1) {
+        print "SAVE FILE : OK\n";
+    }
+    else {
+        print "Can not write";
+    }         
 }
 else {
-    print " Not Successfull";
+    print "please assign url and file name /n";
+    print "Example : perl script/downloadpage.pl http://www.hunsa.com  hunsa.txt /n";
 }
-print "\n";
-if($dlp->write_to_file($filename)==1) {
-    print "SAVE FILE : OK\n";
-}
-else {
-    print "Can not write";
-}         
-
    
