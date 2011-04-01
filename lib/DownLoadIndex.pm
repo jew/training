57d0a1d032d15a1d53611ae89f73f3ee9a5527ca
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-04-01 17:31:18 +0700

    DownLoadIndex.pm#7

--- lib/DownLoadIndex.pm
+++ lib/DownLoadIndex.pm
@@ -4,7 +4,7 @@ use warnings;
 use Moose;
 use LWP::UserAgent;
 use File::Slurp ;
-
+use Data::Dumper;
 has 'url' => ( is => 'rw', isa => 'Str', required => 1, );
 has '_ua' => ( is => 'ro', isa => 'LWP::UserAgent', lazy_build => 1, );
 has 'content' => ( is => 'ro', isa => 'ArrayRef', lazy_build => 1, );
@@ -38,40 +38,41 @@ sub write_to_file {
     }
     write_file($filename,{binmode => ':utf8'},$output);
 }   
-sub update_index {
-    my ($self, $filename) = @_;
-
-
-# #open resault.txt and read line and compare 
-            open(MYINPUTFILE, "<results.txt");
-             my @lines = split /\n/,$_;
-             foreach my $line (@lines) {
-             print "EX-VALUE OF INDEX  :" @lines[$line];
-            
-             }
-#            while(<MYINPUTFILE>)
-#             {
-#             my($line) = $_;
-#                #my @lines = split /\,/,$_;
-#            chomp($line);
-#             print "EX-VALUE OF INDEX  :$line\n";
-#             }
-
-
-#    my @content = @{$self->content};
-#    my $u_index = "";
-#    foreach my $h (@content) {
-#        
-#        $u_index = $h->{name} . "-" . $h->{value} .",". "\n";
-#         print"UPDATE VALUE:$u_index\n";
-#        
-#        
-#    }
-
-
-
+sub update_index{
+    my ($self)   = @_;
+    my $filename = 'results3.txt';     
+    my $text     = read_file( $filename ) ;
+    my @lines    = split /\,/,$text;
+    my $no=$#lines-1;
+    my @all;
+    my $is_changed = 0;
+    # get values from file 
+    for (my $i = 0; $i<=$no ;$i++) {
+        $lines[$i] =~/([\w\s\d\-&\*]+)\-([\d\.]+)/;
+        my $fname=$1;
+        my $fvalue=$2;
+        #get from web
+        my @content = @{$self->content};
+        my $new_v = "";          
+        my $line_w = $content[$i];
+        $fname =~ s/^\s+//;#remove space
+            if (($content[$i]->{name} eq $fname)&&($content[$i]->{value} eq $fvalue)) {           
+                push( @all, { name => $content[$i]->{name} , value => $content[$i]->{value} } );
+            }
+            else {
+                $is_changed = 1;
+                push( @all, { name => $content[$i]->{name} , value => $content[$i]->{value} } );
+            }   
+        }
+    my @content = @{$self->content};
+    my $output = "";
+        foreach my $h (@all) {
+            $output .= $h->{name} . "-" . $h->{value} .",". "\n";
+        }
+    if ( $is_changed ) {
+       write_file($filename,{binmode => ':utf8'},$output);
+    }  
 }
-
 1;
 =head2 _build__au
     
