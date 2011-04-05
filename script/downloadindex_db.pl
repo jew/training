abb5b9b6b6c040d52a1266b51f2f4dd076bedb60
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-04-05 14:38:00 +0700

    created downloadindex_db.pl

---
+++ script/downloadindex_db.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use DownLoadIndex_db;
+use Data::Dumper;
+my $url ='http://www.nasdaq.com/';
+my $dlp = DownLoadIndex_db->new(url => $url);
+$dlp->content;
+$dlp->insert_to_database;
+
+
+
+
